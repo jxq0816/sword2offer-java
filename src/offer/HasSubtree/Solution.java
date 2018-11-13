e7a28ec97be515fe28a55ba49bbb1baa64db619e
jiangxingqi <xingqijiang@sohu-inc.com>
2018-11-13 16:22:01 +0800

    剑指offer

---
+++ src/offer/HasSubtree/Solution.java
@@ -0,0 +1,27 @@
+package offer.HasSubtree;
+
+public class Solution {
+    public boolean HasSubtree(TreeNode root1,TreeNode root2) {
+        if(root1==null || root2==null){
+            return false;
+        }
+        boolean rs=false;
+        if(isSubTree(root1,root2)||isSubTree(root1.left,root2)||isSubTree(root1.right,root2)){
+            rs=true;
+        }
+        return rs;
+    }
+    public boolean isSubTree(TreeNode root1,TreeNode root2){
+        if(root2==null){
+            return true;
+        }
+        if(root1==null){
+            return false;
+        }
+        if(root1.val==root2.val){
+            return (isSubTree(root1.left,root2.left) && isSubTree(root1.right,root2.right));
+        }else{
+            return false;
+        }
+    }
+}
