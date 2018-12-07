c56b8355706733bdfed73cd16206e84edb72992c
jiangxingqi <xingqijiang@sohu-inc.com>
2018-12-07 11:18:18 +0800

    剑指offer

--- src/offer/GetUglyNumber_Solution/Solution.java
+++ src/offer/GetUglyNumber_Solution/Solution.java
@@ -2,33 +2,36 @@ package offer.GetUglyNumber_Solution;
 
 public class Solution {
     public int GetUglyNumber_Solution(int index) {
-        int cnt=0;
-        int i=0;
-        do{
-            i++;
-            int j=i;
-            boolean isUglyNumber=true;
-            while(j!=1&&j!=2&&j!=3&&j!=5){
-                int k=j;
-                if(j%2==0){
-                    j=j/2;
-                }
-                if(j%3==0){
-                    j=j/3;
-                }
-                if(j%5==0){
-                    j=j/5;
-                }
-                if(j==k){
-                    isUglyNumber=false;
-                    break;
-                }
+        if(index==0){
+            return 0;
+        }
+        int[] array=new int[index];
+        array[0]=1;
+        int nextUglyIndex=1;
+        int multiply2 = 0;
+        int multiply3 = 0;
+        int multiply5 = 0;
+        while(nextUglyIndex<index){
+            int min = 0x7fffffff;
+            int mul2=array[multiply2]*2;
+            int mul3=array[multiply3]*3;
+            int mul5=array[multiply5]*5;
+            min=Math.min(min,mul2);
+            min=Math.min(min,mul3);
+            min=Math.min(min,mul5);
+            array[nextUglyIndex] = min;
+            while(array[multiply2]*2<=min){
+                multiply2++;
             }
-            if(isUglyNumber){
-                cnt++;
+            while(array[multiply3]*3<=min){
+                multiply3++;
             }
-        }while(cnt!=index);
-        return i;
+            while(array[multiply5]*5<=min){
+                multiply5++;
+            }
+            nextUglyIndex++;
+        }
+        return array[index-1];
     }
     public static void main(String[] args){
         Solution solution=new Solution();
