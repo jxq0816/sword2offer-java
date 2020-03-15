b8f00a4087770cee19c69edec13e056d5bc6be60
姜兴琪 <xingqijiang@qq.com>
2020-03-15 17:24:47 +0800

    No.4 重建二叉树

--- src/offer/reConstructBinaryTree/Solution.java
+++ src/offer/reConstructBinaryTree/Solution.java
@@ -38,4 +38,12 @@ public class Solution {
         }
         return treeNode;
     }
+    public static void main(String[] args) {
+        //测试
+        int[] pre= {1,2,4,7,3,5,6,8};
+        int[] in= {4,7,2,1,5,3,8,6};
+        Solution solution=new Solution();
+        TreeNode treeNode=solution.reConstructBinaryTree(pre,in);
+        System.out.println(treeNode.toString());
+    }
 }
--- src/offer/tree/TreeNode.java
+++
@@ -1,8 +0,0 @@
-package offer.tree;
-
-public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    public TreeNode(int x) { val = x; }
-}
