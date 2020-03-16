f4eba525e3b353ab53cd24c207c22ba7e178c75d
姜兴琪 <xingqijiang@qq.com>
2020-03-16 20:24:12 +0800

    剑指offer No.7斐波那契数列

--- src/offer/Fibonacci/Solution.java
+++ src/offer/Fibonacci/Solution.java
@@ -1,5 +1,6 @@
+//大家都知道斐波那契数列，现在要求输入一个整数n，请你输出斐波那契数列的第n项（从0开始，第0项为0）。n<=39
+//https://xingqijiang.blog.csdn.net/article/details/104906540
 package offer.Fibonacci;
-
 public class Solution {
     public int Fibonacci(int n) {
         if(n==0){
