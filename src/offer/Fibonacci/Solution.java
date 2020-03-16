a793334d14801b9ede3e88a3cacf39746092a93a
姜兴琪 <xingqijiang@qq.com>
2020-03-16 20:08:07 +0800

    斐波那契数列

---
+++ src/offer/Fibonacci/Solution.java
@@ -0,0 +1,13 @@
+package offer.Fibonacci;
+
+public class Solution {
+    public int Fibonacci(int n) {
+        if(n==0){
+            return 0;
+        }
+        if(n==1 || n==2){
+            return 1;
+        }
+        return Fibonacci(n-1)+Fibonacci(n-2);
+    }
+}
