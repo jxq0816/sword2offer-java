16885ec2c6a5a6e513870649c016009fe7f86c0b
姜兴琪 <xingqijiang@qq.com>
2020-04-01 11:54:13 +0800

    剑指offer No.17 树的子结构

--- src/offer/HasSubtree/Solution.java
+++ src/offer/HasSubtree/Solution.java
@@ -1,5 +1,6 @@
 package offer.HasSubtree;
 
+//输入两棵二叉树A，B，判断B是不是A的子结构。（ps：我们约定空树不是任意一个树的子结构）
 public class Solution {
     public boolean HasSubtree(TreeNode root1,TreeNode root2) {
         if(root1==null || root2==null){
