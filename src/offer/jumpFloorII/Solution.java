695d7929e9f6f5eed62a528ae677de0afabe9715
姜兴琪 <xingqijiang@qq.com>
2020-04-01 12:52:40 +0800

    剑指offer No.9 变态跳台阶

---
+++ src/offer/jumpFloorII/Solution.java
@@ -0,0 +1,13 @@
+package offer.jumpFloorII;
+//一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
+public class Solution {
+    public int JumpFloorII(int target) {
+        if(target==0){
+            return 0;
+        }else if(target==1){
+            return 1;
+        }else{
+            return 2*JumpFloorII(target-1);
+        }
+    }
+}
