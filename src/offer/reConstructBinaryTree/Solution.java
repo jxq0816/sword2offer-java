01092deddcfe97a7eeb4edd4e6c40910fc060181
姜兴琪 <xingqijiang@qq.com>
2020-04-12 18:01:27 +0800

    剑指offer No.4 重建二叉树

--- src/offer/reConstructBinaryTree/Solution.java
+++ src/offer/reConstructBinaryTree/Solution.java
@@ -1,32 +1,34 @@
 package offer.reConstructBinaryTree;
 
 public class Solution {
+    //pre:先序 in:中序
     public TreeNode reConstructBinaryTree(int [] pre,int [] in) {
         TreeNode treeNode=new TreeNode(pre[0]);
-        //获取根节点在中序遍历数组中是位置
-        int RootIndex=0;
+        //查找根节点在先序遍历数组中的下标
+        int rootIndex=0;
         for(int i=0;i<in.length;i++) {
+            //中序遍历数组中的第一个位置是根节点
             if(in[i]==pre[0]) {
-                RootIndex=i;
+                rootIndex=i;
             }
         }
         //将先序的遍历结果分成两份，一份是左子树，一份是右子树
-        int[] preLeft=new int[RootIndex];
-        int[] preRight=new int[pre.length-RootIndex-1];
-        for(int i=0;i<RootIndex;i++) {
+        int[] preLeft=new int[rootIndex];
+        int[] preRight=new int[pre.length-rootIndex-1];
+        for(int i=0;i<rootIndex;i++) {
             preLeft[i]=pre[i+1];
         }
-        for(int i=0;i<pre.length-RootIndex-1;i++) {
-            preRight[i]=pre[RootIndex+1+i];
+        for(int i=0;i<pre.length-rootIndex-1;i++) {
+            preRight[i]=pre[rootIndex+1+i];
         }
         //将中序的遍历的结果分为两份，一份是左子树，一份是右子树
-        int[] midLeft=new int[RootIndex];
-        int[] midRight=new int[in.length-RootIndex-1];
-        for(int i=0;i<RootIndex;i++) {
+        int[] midLeft=new int[rootIndex];
+        int[] midRight=new int[in.length-rootIndex-1];
+        for(int i=0;i<rootIndex;i++) {
             midLeft[i]=in[i];
         }
-        for(int i=0;i<in.length-RootIndex-1;i++) {
-            midRight[i]=in[RootIndex+i+1];
+        for(int i=0;i<in.length-rootIndex-1;i++) {
+            midRight[i]=in[rootIndex+i+1];
         }
         if(in.length>0&pre.length>0) {
             if(preLeft.length>0) {
@@ -38,12 +40,31 @@ public class Solution {
         }
         return treeNode;
     }
+    public void preVisit(TreeNode treeNode){
+        if(treeNode==null){
+            return;
+        }
+        System.out.print(treeNode.val+",");
+        preVisit(treeNode.left);
+        preVisit(treeNode.right);
+    }
+    public void midVisit(TreeNode treeNode){
+        if(treeNode==null){
+            return;
+        }
+        midVisit(treeNode.left);
+        System.out.print(treeNode.val+",");
+        midVisit(treeNode.right);
+    }
     public static void main(String[] args) {
         //测试
         int[] pre= {1,2,4,7,3,5,6,8};
         int[] in= {4,7,2,1,5,3,8,6};
         Solution solution=new Solution();
         TreeNode treeNode=solution.reConstructBinaryTree(pre,in);
-        System.out.println(treeNode.toString());
+        solution.preVisit(treeNode);
+        System.out.println();
+        solution.midVisit(treeNode);
+        System.out.println();
     }
 }
